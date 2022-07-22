373b38356967cd38d9b70e3840e41bed0736c21c
Abhishek <abhishek@pingsafe.com>
2022-07-22 09:06:36 +0530

    add: health check

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/internal/api/v1/now', function(req, res){
 	return res.send({current_time: `Internal time is ${new Date()}`})
 })
 
+app.get('/healthz', function(req, res) {
+	return res.send({healthy: true})
+})
+
 app.listen(port, function() {
 	console.log("Example app listening on port " + port + "!")
 })
